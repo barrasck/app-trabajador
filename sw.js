4374f9c26c6c42cd30609b1325aa86fcbc08fc7e
barrasck <barrascrix@gmail.com>
2025-04-03 15:20:50 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
-self.addEventListener('push', (event) => {
-  const data = event.data.json();
-  const options = {
-    body: data.body,
-    icon: 'icons/icon-192x192.png',
-  };
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
+const CACHE_NAME = 'ama-v1';
+const ASSETS = [
+  '/',
+  '/pasada.html',
+  '/manifest.json',
+  '/icons/icon-maskable-192x192.png'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request)) // (4)
   );
 });
