1bdc6c555b8807d13c161230257412d5a35e4a68
barrasck <barrascrix@gmail.com>
2025-04-03 15:31:29 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -3,16 +3,21 @@ const ASSETS = [
   '/',
   '/pasada.html',
   '/manifest.json',
-  '/icons/icon-maskable-192x192.png'
+  '/icons/icon-maskable-192x192.png', // Ruta corregida
+  '/icons/icon-any-512x512.png' // Añadido ícono "any"
 ];
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS)) // Error de sintaxis corregido
+  );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request)) // (4)
+    fetch(e.request)
+      .catch(() => caches.match(e.request))
   );
 });
