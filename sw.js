56987c4f5ffc2060baf075d685fcb480adc06b48
barrasck <barrascrix@gmail.com>
2025-04-04 07:55:53 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,40 @@
-const CACHE_NAME = 'ama-v1';
+const CACHE_NAME = 'ama-v2'; // Incrementa la versión de la caché
 const ASSETS = [
   '/app-trabajador/',
   '/app-trabajador/pasada.html',
   '/app-trabajador/manifest.json',
   '/app-trabajador/icono-192.png',
-  '/app-trabajador/icono-512.png'
+  '/app-trabajador/icono-512.png',
+  '/app-trabajador/turnos.html',
+  '/app-trabajador/nomina.html',
+  '/app-trabajador/turnos.png',
+  '/app-trabajador/nomina.png',
+  '/app-trabajador/screenshots/pantalla1.jpg'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
