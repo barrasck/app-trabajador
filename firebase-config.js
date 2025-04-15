4044e27dc4f328a8c6ed48372c1f21f463fd2ea6
barrasck <barrascrix@gmail.com>
2025-04-15 13:50:08 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhSUhdGAb9DJw87vSp33CGk7VnSsRxQ60",
+  authDomain: "amalogistic-d2a79.firebaseapp.com",
+  databaseURL: "https://amalogistic-d2a79-default-rtdb.firebaseio.com",
+  projectId: "amalogistic-d2a79",
+  storageBucket: "amalogistic-d2a79.firebasestorage.app",
+  messagingSenderId: "605672630537",
+  appId: "1:605672630537:web:18f7ddb2ad2d23c7becbd8",
+  measurementId: "G-74F64237PN"
+};
+
+firebase.initializeApp(firebaseConfig);
